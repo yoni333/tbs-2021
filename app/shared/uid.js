709bd70ef804ffe9ba8553bf34aace03439b9345
yehonatan yehezkel <yoni333@gmail.com>
2021-11-07 15:39:05 +0200

    remove js. add dist as outDir

--- app/shared/uid.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUID = void 0;
-var uuid_1 = require("uuid");
-var createUID = function () {
-    return (0, uuid_1.v4)();
-};
-exports.createUID = createUID;

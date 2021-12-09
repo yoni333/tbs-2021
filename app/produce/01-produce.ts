bcde007afe171c8265f93f4e143993cc3e398091
Yehonatan Yehezkel <yoni333@gmail.com>
2021-12-09 23:28:50 +0200

    remove abastract getdata

--- app/produce/01-produce.ts
+++ app/produce/01-produce.ts
@@ -12,19 +12,3 @@ export class Produce {
   }
 }
 
-// const a=new Produce(EProduce.iron);
-
-
-// export class Produce {
-//   _type;
-//   constructor(type) {
-//     this.type=type
-//    }
-//   private set type (type){
-//     console.log(type);
-    
-//     this._type=type
-//   }
-// }
-
-// const a=new Produce('iron');
--- app/shared/data.ts
+++ app/shared/data.ts
@@ -4,33 +4,33 @@ export enum ClassTypes {
   // do not make changes to this list - without make DB normalization.
   // you can only add to this list with out DB normalization
   Metadata = 2,
-  city = 10,
-  CityDetails = 11,
-  CityDistrict = 12,
-  Resources = 13,
+  city = 20,
+  CityDetails = 21,
+  CityDistrict = 22,
+  Resources = 23,
 }
 
-export interface IData<T> {
-  data: T;
-  getData(): T;
-}
 
-export abstract class Data<T> implements IData<T> {
+
+export abstract class Data<T>  {
   protected _data!: T
   private _type: ClassTypes;
 
   constructor(type: ClassTypes,data:T) {
     this._type = type;
-    this._data=data
+    this.data = data
 
   }
   get data(): T {
     return this._data;
   }
+  set data(data:T){
+    this._data=data
+  }
   get type(): ClassTypes {
     return this._type;
   }
-  abstract getData(): T;
+  // abstract getData(): T;
 
   error({ message, action, caller }: IErrorDataBase) {
     ErrorData.error({ message, caller, class: this.type, action: action });

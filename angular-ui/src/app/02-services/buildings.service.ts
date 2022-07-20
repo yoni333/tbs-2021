ae051a40d2b0ee92c693479f20923f91e7f71bf9
yehonatan yehezkel <yoni333@gmail.com>
2022-07-21 00:38:42 +0300

    add routing basic

--- angular-ui/src/app/02-services/buildings.service.ts
+++ angular-ui/src/app/02-services/buildings.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ClassTypes } from "../../../../app/shared/data";
 import {Building, EBuildingName}from '../01-domain/building'
-@Injectable()
+@Injectable({providedIn:'root'})
 export class BuildingService{
     constructor(){
 
--- angular-ui/src/app/app-routing.module.ts
+++ angular-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuildingComponent } from './building/building.component';
+import { CityComponent } from './city/city.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'city',component:CityComponent},
+  {path:'building',component:BuildingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
